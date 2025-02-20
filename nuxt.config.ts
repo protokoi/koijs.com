deec1aeaf2653413a2d0b867428391294e040f08
eiskaltDEV <atlasyigitaydin@gmail.com>
2025-02-20 17:52:35 +0300

    add cname and 8080

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+import process from 'node:process'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
@@ -11,6 +12,12 @@ export default defineNuxtConfig({
         class: 'dark',
       },
     },
+  },
+  nitro: {
+    preset: 'node',
+  },
+  devServer: {
+    port: Number.parseInt(process.env.PORT || '8080'),
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
